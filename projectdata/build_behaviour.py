dc0c5fc9b7a723c5950aee46e1aaabeb7a11775a
David Drysdale-Wilson <david.drysdale-wilson@linn.co.uk>
2015-04-17 11:58:57 +0100

    Initial commit.

---
+++ projectdata/build_behaviour.py
@@ -0,0 +1,33 @@
+# Defines the build behaviour for continuous integration builds.
+
+import sys
+import os
+import shutil
+import glob
+
+try:
+    from ci import (OpenHomeBuilder, require_version)
+except ImportError:
+    print "You need to update ohDevTools."
+    sys.exit(1)
+
+require_version(45)
+
+
+class Builder(OpenHomeBuilder):
+    output_dir = "build/packages"
+
+    def setup(self):
+        self.nuget_server = self.env.get('NUGET_SERVER')
+        self.nuget_api_key = self.env.get('NUGET_API_KEY')
+
+    def clean(self):
+        if os.path.isdir(self.output_dir):
+            shutil.rmtree(self.output_dir)
+
+    def build(self):
+        os.makedirs(self.output_dir)
+        self.pack_nuget('src/ohXen.nuspec', '.')
+
+    def publish(self):
+        self.publish_nuget(os.path.join('build', 'packages', '*.nupkg'), self.nuget_api_key, self.nuget_server)
